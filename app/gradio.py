183c25f866f0ca5fc7a0787dd31e426cdbd8391e
jKyuery <kyugenes17@gmail.com>
2023-05-16 16:26:54 -0500

    started web app

---
+++ app/gradio.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import tensorflow_hub as tfh
+
+import gradio as gr
+import requests
+
+model = tf.keras.models.load_model(
+    ("final_model.h5"), custom_objects = {'KerasLayer': tfh.KerasLayer}
+)
+
+labels = ["Green", "Blue", "Red", "Airport", "Night bus", "Other"]
+
+def classify_image(inp):
+    inp = inp.reshape((-1, 224, 224, 3))
+    inp = inp / 255
+
+    pred = model.predict(inp).flatten()
+    confidence = {labels[i]: float(pred[i]) for i in range(len(labels))}
+    return confidence
+
+app = gr.Interfact(fn = classify_image,
+                   inputs = gr.Image(shape=(224, 224)), 
+                   outputs = gr.label(num_top_classes = 3)
+)
+
+app.launch()
